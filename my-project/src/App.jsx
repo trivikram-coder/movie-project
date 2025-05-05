d75cfe44f1825a5e1d03503cee48606e529a396e
vikram-42-alla <allatrivikram@gmail.com>
2025-05-05 20:24:47 +0530

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Movie from './Component/Movie'
+import './App.css'
+const App = () => {
+  return (
+    <div>
+      <Movie/>
+    </div>
+  )
+}
+
+export default App
